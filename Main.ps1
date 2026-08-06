c1f8ddd42c9f805bbf220fd013a7a579077df2c3
Thiago Sathler <thiago.sathler6@gmail.com>
2026-08-06 19:48:20 -0300

    Create initial project structure

---
+++ Main.ps1
@@ -0,0 +1,11 @@
+# ============================================================================
+# EPSetup
+# Main entry point
+# Author: Thiago Sathler
+# ============================================================================
+
+# Carrega os módulos
+. ".\Modules\UI\Banner.ps1"
+
+# Início da aplicação
+Show-Banner
